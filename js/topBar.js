f019d447f7219c6d68ab5d5321cec675b5ed2017
Liam St. Ores <liam.st.ores@gmail.com>
2022-10-24 18:47:39 -0500

    hopefully fixes some stuff

--- js/topBar.js
+++
@@ -1,8 +0,0 @@
-fetch("js/files/topBar.html")
-.then((response) => response.text())
-.then((html) => {
-    document.getElementById("topBar").innerHTML = html;
-})
-.catch((error) => {
-    console.warn(error);
-});
