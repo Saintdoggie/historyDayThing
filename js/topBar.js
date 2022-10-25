094b7f39ae2924687e522ec7ba8130d36ca66b54
Liam St. Ores <liam.st.ores@gmail.com>
2022-10-25 18:54:05 -0500

    image cool thing

--- js/topBar.js
+++ js/topBar.js
@@ -6,3 +6,7 @@ fetch("js/files/topBar.html")
 .catch((error) => {
     console.warn(error);
 });
+// var element = document.getElementById('topBar');
+// element.style.opacity = "0";
+// var elements = document.getElementsByClassName('TheTopBarmenus');
+// elements.style.opacity = "1";
