1c0da7536db93d566f4755f7111fef2a4480caec
Liam St. Ores <liam.st.ores@gmail.com>
2022-10-26 19:39:34 -0500

    some home page requirements

--- js/topBar.js
+++ js/topBar.js
@@ -6,7 +6,3 @@ fetch("js/files/topBar.html")
 .catch((error) => {
     console.warn(error);
 });
-// var element = document.getElementById('topBar');
-// element.style.opacity = "0";
-// var elements = document.getElementsByClassName('TheTopBarmenus');
-// elements.style.opacity = "1";
