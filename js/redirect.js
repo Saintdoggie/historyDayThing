be9206d8155d06a248fb4747caa73848e8025256
Liam St.Ores <liam.st.ores@gmail.com>
2022-11-01 08:23:46 -0500

    Update redirect.js

--- js/redirect.js
+++ js/redirect.js
@@ -1,12 +1,12 @@
 var page = "home";
 function redirect(toWhere) {
     if (toWhere == "unixBeginning") {
-        window.location.replace("unix/beginning");
+        window.location.replace("/historyDayThing/unix/beginning");
         page = "unixBeginning"
         
     }
     if (toWhere == "home") {
-        window.location.replace("home")
+        window.location.replace("/historyDayThing/home")
         page = "home"
     }
     else {
