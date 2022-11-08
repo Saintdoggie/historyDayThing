74131022ba6ec56cde6e28dbc6488acdbccdefc5
Liam St. Ores <liam.st.ores@gmail.com>
2022-11-07 18:23:44 -0600

    trying to get github pages, and my computer to work

--- js/redirect.js
+++ js/redirect.js
@@ -1,13 +1,27 @@
 var page = "home";
 function redirect(toWhere) {
     if (toWhere == "unixBeginning") {
-        window.location.replace("/unix/beginning");
-        page = "unixBeginning"
+        if (page == "unixBeginning") {
+            window.location.replace("../unix/beginning");
+            page = "unixBeginning"
+
+        }
+        else if (page == "home") {
+            window.location.replace("../unix/beginning");
+            page = "unixBeginning"
+        }
         
     }
     if (toWhere == "home") {
-        window.location.replace("/home")
-        page = "home"
+        if (page == "unixBeginning") {
+            window.location.replace("../home");
+            page = "home"
+
+        }
+        else if (page == "home") {
+            window.location.replace("../home");
+            page = "unixBeginning"
+        }
     }
     else {
         console.log("no place to go")
